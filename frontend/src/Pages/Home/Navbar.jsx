3303c59eef8a303639f5d75a2936ea96c906ed80
Md. Shoriful Islam <sisazid182713@gmail.com>
2025-02-18 11:49:29 +0600

    navbar search functionality added

--- frontend/src/Pages/Home/Navbar.jsx
+++ frontend/src/Pages/Home/Navbar.jsx
@@ -7,6 +7,7 @@ import React from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <section className="font-Jost uppercase tracking-widest">
@@ -81,8 +82,21 @@ const Navbar = () => {
 
           {/* Right Side Icons */}
           <div className="hidden md:flex items-center gap-5">
-            <span className="inline-flex items-center gap-1 text-sm">
-              <MdSearch size={20} /> Search
+            <span
+              className="inline-flex items-center gap-1 text-sm"
+              onClick={() => setIsSearchOpen(!isSearchOpen)}
+            >
+              {isSearchOpen ? (
+                <span className="inline-flex gap-4 cursor-pointer">
+                  <RxCross2 size={20} />
+                  Close
+                </span>
+              ) : (
+                <span className="inline-flex gap-1 cursor-pointer">
+                  <MdSearch size={20} />
+                  Search
+                </span>
+              )}
             </span>
             <div className="md:flex space-x-5">
               <MdFacebook size={20} color="DB915E" />
@@ -96,39 +110,53 @@ const Navbar = () => {
 
       {/* Main Section - Desktop */}
       <div className="hidden md:flex my-24 justify-between items-center w-5/6 mx-auto">
-        <div className="flex flex-col items-center gap-2">
-          <img
-            src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/10/rainbow.png"
-            alt="New In"
-          />
-          NEW IN
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <img
-            src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/10/star.png"
-            alt="About Us"
-          />
-          ABOUT US
-        </div>
-        <img
-          src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/11/logo-img-1-300x91.png"
-          alt="PlayGrow Logo"
-          className="w-[150px]"
-        />
-        <div className="flex flex-col items-center gap-2">
-          <img
-            src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/10/hearth.png"
-            alt="Wishlist"
-          />
-          WISHLIST
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <img
-            src="https://playgrow.qodeinteractive.com/wp-content/plugins/playgrow-core/assets/img/cart-empty-large.png"
-            alt="Cart"
-          />
-          CART $0.00
-        </div>
+        <>
+          {isSearchOpen ? (
+            <div className="my-6 w-full">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="border-b-2 border-gray-400 outline-none p-2 text-3xl w-full" 
+              />
+            </div>
+          ) : (
+            <>
+              <div className="flex flex-col items-center gap-2">
+                <img
+                  src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/10/rainbow.png"
+                  alt="New In"
+                />
+                NEW IN
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <img
+                  src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/10/star.png"
+                  alt="About Us"
+                />
+                ABOUT US
+              </div>
+              <img
+                src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/11/logo-img-1-300x91.png"
+                alt="PlayGrow Logo"
+                className="w-[150px]"
+              />
+              <div className="flex flex-col items-center gap-2">
+                <img
+                  src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/10/hearth.png"
+                  alt="Wishlist"
+                />
+                WISHLIST
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <img
+                  src="https://playgrow.qodeinteractive.com/wp-content/plugins/playgrow-core/assets/img/cart-empty-large.png"
+                  alt="Cart"
+                />
+                CART $0.00
+              </div>
+            </>
+          )}
+        </>
       </div>
 
       {/* Mobile Header */}
