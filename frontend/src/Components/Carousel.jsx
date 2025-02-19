0a94ee0b0ddbf6168014945ffc61ae78a3e644cf
Md. Shoriful Islam <sisazid182713@gmail.com>
2025-02-20 04:39:03 +0600

    carousels 3rd slide responsiveness done

--- frontend/src/Components/Carousel.jsx
+++ frontend/src/Components/Carousel.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 
-// import required modules
 import { Autoplay, EffectFade, Navigation } from "swiper/modules";
+import Rating from "react-rating";
+import { FaRegStar } from "react-icons/fa";
+import { MdStar } from "react-icons/md";
 
 export default function Carousel() {
   return (
@@ -40,11 +40,30 @@ export default function Carousel() {
           />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://i.ibb.co.com/Zzt0d4LY/image-Photoroom.png"
-            className="bg-cover bg-center lg:h-[550px] h-[500px] w-full object-cover"
-            alt="Slide-3"
-          />
+          <div className="relative">
+            <img
+              src="https://playgrow.qodeinteractive.com/wp-content/uploads/2023/01/main-home-rev-img-03.jpg"
+              className="bg-cover bg-center lg:h-[550px] h-[500px] w-full object-cover"
+              alt="Slide-3"
+            />
+            <div className="absolute md:top-52 top-44 right-1 md:right-80 bg-white rounded-lg md:w-[480px] w-[330px] text-center md:py-10 py-6">
+              <p>
+                <Rating
+                  initialRating={5}
+                  emptySymbol={
+                    <FaRegStar size={30} style={{ color: "#ccc" }} />
+                  }
+                  fullSymbol={<MdStar size={30} style={{ color: "#DB915E" }} />}
+                />
+              </p>
+              <h1 className="md:text-3xl text-2xl uppercase tracking-widest md:my-3 mt-2 text-gray-700 font-medium">
+                Welcome to PlayGrow
+              </h1>
+              <p className="text-gray-500 md:block hidden">
+                Buy your beloved baby's happiness here
+              </p>
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
